1b1bbbf451c91c2bab1c1ffcce5ecb9ce21cd734
Salman Zuberi Hussain <96196317+salman1hasan@users.noreply.github.com>
2023-05-31 19:17:59 -0400

    Add files via upload

---
+++ src/components/Logo.js
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+import React from 'react';
+import { motion } from 'framer-motion';
+
+const MotionLink = motion(Link);
+
+const Logo = () => {
+  return (
+    <div className="flex items-center justify-center mt-2">
+      <MotionLink
+        href="/"
+        className="w-16 h-16 bg-orange-400 text-light flex items-center justify-center rounded-full text-2xl font-bold"
+        whileHover={{
+          backgroundColor: ['#121212'],
+          transition: { duration: 1, repeat: Infinity },
+        }}
+      >
+        SH
+      </MotionLink>
+    </div>
+  );
+};
+
+export default Logo;
