3af709c8ea4200ff081a2941376c349616bc356c
Salman Zuberi Hussain(Sal Hasan) <96196317+salman1hasan@users.noreply.github.com>
2023-06-10 07:25:17 -0400

    Add files via upload

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+
+const Skill = ({ name, x, y }) => {
+  return (
+    <motion.div
+      className="flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 shadow-dark cursor-pointer absolute"
+      whileHover={{ scale: 1.05 }}
+    >
+      {name}
+    </motion.div>
+  );
+};
 
 const Skills = () => {
   return (
     <>
       <h2 className="font-bold text-8xl mt-64 w-full text-center">Skills</h2>
       <div className="w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight">
-        <div className="flex items-center justify-center rounded-full font-semibold bg-dark text-light">
+        <motion.div
+          className="flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 shadow-dark cursor-pointer"
+          whileHover={{ scale: 1.05 }}
+        >
           Web
-        </div>
+        </motion.div>
+        <Skill name="CSS" x="-5w" y="-10w" />
       </div>
     </>
   );
